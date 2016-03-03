b0c348f704ede590da2846cfaf973bf333a33157
Viktor Suprun <popsul1993@gmail.com>
2016-03-03 19:48:02 +1000

    initial commit

---
+++ ngx_cache_status_module.c
@@ -0,0 +1,174 @@
+
+/*
+ * Copyright (C) Viktor Suprun
+ */
+
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+
+static ngx_atomic_uint_t cache_status[] = {
+    0, //NGX_HTTP_CACHE_MISS,
+    0, //NGX_HTTP_CACHE_BYPASS
+    0, //NGX_HTTP_CACHE_EXPIRED
+    0, //NGX_HTTP_CACHE_STALE
+    0, //NGX_HTTP_CACHE_UPDATING
+    0, //NGX_HTTP_CACHE_REVALIDATED
+    0, //NGX_HTTP_CACHE_HIT
+    0  //NGX_HTTP_CACHE_SCARCE
+};
+
+static ngx_http_output_header_filter_pt  ngx_original_filter_ptr;
+
+static char *ngx_set_status(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static ngx_int_t ngx_cache_status_filter(ngx_http_request_t *r);
+static ngx_int_t ngx_cache_status_filter_init(ngx_conf_t *cf);
+
+static ngx_command_t  ngx_status_commands[] = {
+
+    { ngx_string("cache_status"),
+      NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS|NGX_CONF_TAKE1,
+      ngx_set_status,
+      0,
+      0,
+      NULL },
+
+      ngx_null_command
+};
+
+
+static ngx_http_module_t  ngx_cache_status_module_ctx = {
+    NULL,				   /* preconfiguration */
+    ngx_cache_status_filter_init,          /* postconfiguration */
+
+    NULL,                                  /* create main configuration */
+    NULL,                                  /* init main configuration */
+
+    NULL,                                  /* create server configuration */
+    NULL,                                  /* merge server configuration */
+
+    NULL,                                  /* create location configuration */
+    NULL                                   /* merge location configuration */
+};
+
+
+ngx_module_t  ngx_cache_status_module = {
+    NGX_MODULE_V1,
+    &ngx_cache_status_module_ctx,      /* module context */
+    ngx_status_commands,              /* module directives */
+    NGX_HTTP_MODULE,                       /* module type */
+    NULL,                                  /* init master */
+    NULL,                                  /* init module */
+    NULL,                                  /* init process */
+    NULL,                                  /* init thread */
+    NULL,                                  /* exit thread */
+    NULL,                                  /* exit process */
+    NULL,                                  /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
+static ngx_int_t ngx_status_handler(ngx_http_request_t *r)
+{
+    size_t             size;
+    ngx_int_t          rc;
+    ngx_buf_t         *b;
+    ngx_chain_t        out;
+
+    if (r->method != NGX_HTTP_GET && r->method != NGX_HTTP_HEAD) {
+        return NGX_HTTP_NOT_ALLOWED;
+    }
+
+    rc = ngx_http_discard_request_body(r);
+
+    if (rc != NGX_OK) {
+        return rc;
+    }
+
+    r->headers_out.content_type_len = sizeof("text/plain") - 1;
+    ngx_str_set(&r->headers_out.content_type, "text/plain");
+    r->headers_out.content_type_lowcase = NULL;
+
+    if (r->method == NGX_HTTP_HEAD) {
+        r->headers_out.status = NGX_HTTP_OK;
+
+        rc = ngx_http_send_header(r);
+
+        if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+            return rc;
+        }
+    }
+   
+    size = sizeof("Cache statistics:\n")
+	    + sizeof("Miss: \n")	+ NGX_ATOMIC_T_LEN
+	    + sizeof("Bypass: \n")	+ NGX_ATOMIC_T_LEN
+	    + sizeof("Expired: \n")	+ NGX_ATOMIC_T_LEN
+	    + sizeof("Stale: \n")	+ NGX_ATOMIC_T_LEN
+	    + sizeof("Updating: \n")	+ NGX_ATOMIC_T_LEN
+	    + sizeof("Revalidated: \n") + NGX_ATOMIC_T_LEN
+	    + sizeof("Hit: \n")		+ NGX_ATOMIC_T_LEN
+	    + sizeof("Scarce: ")	+ NGX_ATOMIC_T_LEN;
+
+    b = ngx_create_temp_buf(r->pool, size);
+    if (b == NULL) {
+        return NGX_HTTP_INTERNAL_SERVER_ERROR;
+    }
+
+    out.buf = b;
+    out.next = NULL;
+    
+    b->last = ngx_cpymem(b->last, "Cache statistics:\n", sizeof("Cache statistics:\n") - 1);
+    b->last = ngx_sprintf(b->last, "Miss: %uA\n",	    cache_status[0]);
+    b->last = ngx_sprintf(b->last, "Bypass: %uA\n",	    cache_status[1]);
+    b->last = ngx_sprintf(b->last, "Expired: %uA\n",	    cache_status[2]);
+    b->last = ngx_sprintf(b->last, "Stale: %uA\n",	    cache_status[3]);
+    b->last = ngx_sprintf(b->last, "Updating: %uA\n",	    cache_status[4]);
+    b->last = ngx_sprintf(b->last, "Revalidated: %uA\n",    cache_status[5]);
+    b->last = ngx_sprintf(b->last, "Hit: %uA\n",	    cache_status[6]);
+    b->last = ngx_sprintf(b->last, "Scarce: %uA",	    cache_status[7]);
+
+    r->headers_out.status = NGX_HTTP_OK;
+    r->headers_out.content_length_n = b->last - b->pos;
+
+    b->last_buf = (r == r->main) ? 1 : 0;
+    b->last_in_chain = 1;
+
+    rc = ngx_http_send_header(r);
+
+    if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
+        return rc;
+    }
+
+    return ngx_http_output_filter(r, &out);
+}
+
+static char *ngx_set_status(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_core_loc_conf_t  *clcf;
+
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_status_handler;
+
+    return NGX_CONF_OK;
+}
+
+static ngx_int_t ngx_cache_status_filter_init(ngx_conf_t *cf)
+{   
+    ngx_original_filter_ptr = ngx_http_top_header_filter;
+    ngx_http_top_header_filter = ngx_cache_status_filter;
+    
+    return NGX_OK;
+}
+
+static ngx_int_t ngx_cache_status_filter(ngx_http_request_t *r) 
+{
+#if (NGX_HTTP_CACHE)
+    if (r->upstream == NULL || r->upstream->cache_status == 0) {
+        return ngx_original_filter_ptr(r);
+    }
+    
+    cache_status[r->upstream->cache_status-1]++;
+#endif
+    
+    return ngx_original_filter_ptr(r);
+}
